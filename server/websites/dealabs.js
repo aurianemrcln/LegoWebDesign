e46e5178507d5556f58df53e12a160edb5879032
Auriane Marcelino <auriane.marcelino@gmail.com>
2025-03-10 18:40:30 +0100

    Scrape Deals from dealabs.com

--- server/websites/dealabs.js
+++ server/websites/dealabs.js
@@ -6,36 +6,52 @@ const cheerio = require('cheerio');
  * @param  {String} data - html response
  * @return {Object} deal
  */
+
+
+function formatImage(imageUrl) {
+    // Exemple : Ajouter un prefixe ou transformer l'URL
+    return imageUrl ? `https://cdn.example.com/${imageUrl}` : null;
+}
+
 const parse = data => {
-  const $ = cheerio.load(data, {'xmlMode': true});
+    const $ = cheerio.load(data, { 'xmlMode': true });
 
-  return $('div.listLayout-main article')
-    .map((i, element) => {
-      const price = parseFloat(
-        $(element)
-          .find('span.vAlign--all-tt')
-          .text()
-      );
+    return $('div.js-threadList article')
+        .map((i, element) => {
+            const link = $(element)
+                .find('a[data-t="threadLink"]')
+                .attr('href');
 
-      const discount = Math.abs(parseInt(
-        $(element)
-          .find('span.textBadge--green')
-          .text()
-      ));
+            const data = JSON.parse($(element)
+                    .find('div.js-vue2')
+                    .attr('data-vue2'));
 
-      const title = 
-        $(element)
-          .find('strong.thread-title')
-          .text()
-      ;
+            const thread = data.props.thread;
+            const retail = thread.nextBestPrice;
+            const price = thread.price;
+            const discount = parseInt((retail - price) / retail * 100)
+            const temperature = + thread.temperature;
+            const photo = thread.mainImage || null; //formatImage(thread.mainImage);
+            const comments = +thread.commentCount;
+            const published = thread.publishedAt;
+            const title = thread.title;
+            const id = thread.threadId;
 
-      return {
-        discount,
-        price,
-        title
-      };
-    })
-    .get();
+
+            return {
+                link,
+                retail,
+                price,
+                discount,
+                temperature,
+                photo,
+                comments,
+                published,
+                title,
+                id
+            };
+        })
+        .get();
 };
 
 /**
@@ -44,15 +60,19 @@ const parse = data => {
  * @returns 
  */
 module.exports.scrape = async url => {
-  const response = await fetch(url);
+    const response = await fetch(url, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        }
+    });
 
-  if (response.ok) {
-    const body = await response.text();
+    if (response.ok) {
+        const body = await response.text();
 
-    return parse(body);
-  }
+        return parse(body);
+    }
 
-  console.error(response);
+    console.error(response);
 
-  return null;
+    return null;
 };
